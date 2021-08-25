20195703f9f302453eb14bf3dd6949166ea77467
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-08-25 13:22:49 +0900

    天気のカラムをeunm で設定

--- app/models/worship.rb
+++ app/models/worship.rb
@@ -3,5 +3,6 @@ class Worship < ApplicationRecord
   belongs_to :shinto
   has_many :worship_params, dependent: :destroy
   accepts_nested_attributes_for :worship_params
+  enum weather: {晴れ: 0, 曇り: 1, 雨: 2, 雪: 3}
 end
 
