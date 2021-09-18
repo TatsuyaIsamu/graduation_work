bad4ac115dfe168c42364f7884db2571a69caa28
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-09-18 17:04:46 +0900

    jsファイルのリファクタリング

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,39 +30,15 @@
 //= require Chart.bundle
 //= require_tree .
 
-
+// datepicker drawer bscustomfileinput
 $(document).on('turbolinks:load', function () {
     $('.drawer').drawer();
     $('.datepicker').datepicker({ dateFormat: 'yy-mm-dd' })
     bsCustomFileInput.init();
-    $('.evaluate_stars').raty({
-        starOn: "/raty/star-on.png" ,
-        starOff: "/raty/star-off.png",
-        starHalf: "/raty/star-half.png ",
-        scoreName: 'worship[worship_params_attributes][0][points]',
-        half: true,
-    });
-    $('.evaluate').raty({
-        starOn: "/raty/star-on.png" ,
-        starOff: "/raty/star-off.png",
-        starHalf: "/raty/star-half.png ",
-        scoreName: 'shinto_user_param[shinto_params_attributes][0][shinto_param_items_attributes][0][points]',
-        half: true,
-    });
 })
 
+// 御朱印帳 
 document.addEventListener("turbolinks:load", function(){
-    $("#worships")
-    .on('cocoon:after-insert', function(e, insertedItem) {
-        $(insertedItem).find(".evaluate_stars").raty(
-        {
-            starOn: "/raty/star-on.png" ,
-            starOff: "/raty/star-off.png",
-            starHalf: "/raty/star-half.png ",
-            scoreName: `worship[worship_params_attributes][${insertedItem[0].children[0].firstChild.lastElementChild.name.match(/[0-9]{12,}/)[0]}][points]`,
-            half: true,
-        });
-    })
     $('#magazine').turn({
         elevation: 50,
         duration: 1000,
@@ -72,28 +48,37 @@ document.addEventListener("turbolinks:load", function(){
     });
 })
 
-
-
+// 参拝画面の星フォーム
 document.addEventListener("turbolinks:load", function(){
-    let item_id = gon.star_array
-    debugger
-    item_id.forEach((item_id) => {
-        let key = Object.keys(item_id)[0]
-        $(`#${key}`).raty({
+    $('.worship_stars').raty({
+        starOn: "/raty/star-on.png" ,
+        starOff: "/raty/star-off.png",
+        starHalf: "/raty/star-half.png ",
+        scoreName: 'shinto_user_param[shinto_params_attributes][0][shinto_param_items_attributes][0][points]',
+        half: true,
+    });
+    $("#worship_params")
+    .on('cocoon:after-insert', function(e, insertedItem) {
+        $(insertedItem).find(".worship_stars").raty(
+        {
             starOn: "/raty/star-on.png" ,
             starOff: "/raty/star-off.png",
             starHalf: "/raty/star-half.png ",
-            score: item_id[key],
-            readOnly: true,
+            scoreName: `worship[worship_params_attributes][${insertedItem[0].children[0].firstChild.lastElementChild.name.match(/[0-9]{12,}/)[0]}][points]`,
             half: true,
         });
     })
+})
 
-});
-
-
-
+// 神社詳細画面の星のフォーム
 document.addEventListener("turbolinks:load", function(){
+    $('.evaluate').raty({
+        starOn: "/raty/star-on.png" ,
+        starOff: "/raty/star-off.png",
+        starHalf: "/raty/star-half.png ",
+        scoreName: 'shinto_user_param[shinto_params_attributes][0][shinto_param_items_attributes][0][points]',
+        half: true,
+    });
     $("#shinto_params")
     .on('cocoon:after-insert', function(e, insertedItem) {
         $(insertedItem).find(".evaluate").raty(
@@ -106,3 +91,20 @@ document.addEventListener("turbolinks:load", function(){
         });
     })
 })
+
+
+// 神社詳細画面の星一覧
+document.addEventListener("turbolinks:load", function(){
+    let item_id = gon.star_array
+    item_id.forEach((item_id) => {
+        let key = Object.keys(item_id)[0]
+        $(`#${key}`).raty({
+            starOn: "/raty/star-on.png" ,
+            starOff: "/raty/star-off.png",
+            starHalf: "/raty/star-half.png ",
+            score: item_id[key],
+            readOnly: true,
+            half: true,
+        });
+    })
+});
