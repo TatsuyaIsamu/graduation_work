240a0bc7484ae5c4b2ca5cd2452a41ab4f166f5c
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-09-18 23:38:32 +0900

    いいね機能を実装 他のユーザーがアクセスしたときにはきちんとそのユーザーの参拝一覧に戻るように設定

--- app/controllers/favorite_worships_controller.rb
+++ app/controllers/favorite_worships_controller.rb
@@ -2,41 +2,19 @@ class FavoriteWorshipsController < ApplicationController
   before_action :authenticate_user!
   respond_to? :js
   def create
-    @worship = Worship.find(params[:worship_id])
-    favorite = current_user.favorite_worships.build(params[:worship_id])
+    @worship = Worship.find(params[:id])
+    favorite = current_user.favorite_worships.build(worship_id: params[:id])
     if favorite.save
       render :favorite_worship
     end
   end
 
   def destroy
-    @worship = Worship.find(params[:worship_id])
-    favorite = current_user.favorites.find_by(worship_id: params[:worship_id])
+    @worship = Worship.find(params[:id])
+    favorite = current_user.favorite_worships.find_by(worship_id: params[:id])
     favorite.destroy
     render :favorite_worship
   end
-  # def create
-  #   @favorite_worship = FavoriteWorship.new(favorite_worship_params)
-
-  #   respond_to do |format|
-  #     if @favorite_worship.save
-  #       format.html { redirect_to @favorite_worship, notice: "Favorite worship was successfully created." }
-  #       format.json { render :show, status: :created, location: @favorite_worship }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @favorite_worship.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # def destroy
-  #   @favorite_worship = FavoriteWorship.find(params[:id])
-  #   @favorite_worship.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to favorite_worships_url, notice: "Favorite worship was successfully destroyed." }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   private
     def favorite_worship_params
