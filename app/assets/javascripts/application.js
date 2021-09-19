695aaeb86e5ebc298fb093c37649ae3f7f6a6db1
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-09-20 00:04:15 +0900

    table のcss調整 tr をクリックしたときにリンクを飛ばす

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -109,3 +109,16 @@ document.addEventListener("turbolinks:load", function(){
         });
     })
 });
+
+// table の列をクリックしたときに リンクを発火させる
+document.addEventListener("turbolinks:load", function(){
+    $('tbody tr[data-href]').addClass('clickable').click( function() {
+        window.location = $(this).attr('data-href');
+    }).find('a').hover( function() {
+        $(this).parents('tr').unbind('click');
+    }, function() {
+        $(this).parents('tr').click( function() {
+            window.location = $(this).attr('data-href');
+        });
+    });
+});
