6e9e25fc0c4e5b4e96252ee0645f00694bd16751
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-02 14:56:28 +0900

    プロフィール更新の際のflash を変更

--- app/controllers/user_introductions_controller.rb
+++ app/controllers/user_introductions_controller.rb
@@ -36,15 +36,8 @@ class UserIntroductionsController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user_introduction.update(user_introduction_params)
-        format.html { redirect_to @user_introduction, notice: "User introduction was successfully updated." }
-        format.json { render :show, status: :ok, location: @user_introduction }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user_introduction.errors, status: :unprocessable_entity }
-      end
-    end
+    @user_introduction.update(user_introduction_params)
+    redirect_to @user_introduction, notice: "プロフィールを更新しました"
   end
 
   private
