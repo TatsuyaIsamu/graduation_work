042b69d6d834df569b650e07d04fe08075dbcfe9
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-06 17:05:31 +0900

    a!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :trim_shinto
   layout :layout_by_resource
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
