bc5fab855688c759b09f09ee7f0455de947d4a8d
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-08 14:26:39 +0900

    shintoのモデルテストを作成

--- spec/models/shinto_spec.rb
+++ spec/models/shinto_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe Shinto, type: :model do
   describe '神社の登録' do
     context '神社の登録をしたとき' do
       it '神社が登録できる' do
-        shinto = FactoryBot.create(:shinto)
-        binding.irb
-        expect(shinto).to be_valid
+        expect(FactoryBot.create(:shinto)).to be_valid
       end
     end
   end
-  
+  describe 'geocoder' do
+    context '住所を登録したとき' do
+      it '緯度と経度が算出される' do
+        expect(FactoryBot.create(:shinto).latitude).not_to be_blank
+      end
+    end
+  end
 end
