8ef491f3e998322dd794af32e2e2e1d48966d0b1
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-09 00:13:46 +0900

    message モデルのモデルテストを作成

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:message) {FactoryBot.create(:message)}
+  describe 'Message モデル' do
+    it  ' インスタンスが作成できる' do
+      expect(message).to be_valid
+    end
+    context 'bodyが空のとき' do
+      it  'バリデーションに引っかかる' do
+        message.body = nil
+        expect(message).to be_invalid
+      end
+    end
+  end
 end
