a3cd98317f72aa4f81bafd1b042b313059f6979d
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-10 21:19:27 +0900

    loginモジュールをつかってuser_spec のリファクタリング

--- spec/system/user_introduction_spec.rb
+++ spec/system/user_introduction_spec.rb
@@ -1,12 +1,14 @@
 RSpec.describe UserIntroduction, type: :system do
   describe "ユーザープロフィールの編集機能" do
+    let(:user){create(:user)}
     before do
-      create(:user)      
+      user    
       user_login
     end
     context "ユーザーが住所を青森県に変更したとき" do
       it "変更できる" do
-        visit home_path
+        
+
       end
     end
   end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :system do
         fill_in "user[email]", with: "test@com"
         fill_in "user[password]", with: "password"
         click_on "アカウント登録"
-        expect(page).to have_content "新しい投稿"
+        expect(page).to have_content "アカウント登録が完了しました"
       end
     end
     context "ログインしていないユーザーがトップ画面にアクセスしたとき" do
@@ -23,27 +23,21 @@ RSpec.describe User, type: :system do
         fill_in "user[email]", with: "test@gmail"
         fill_in "user[password]", with: "111111"
         click_on "commit"
-        expect(page).to have_content "新しい投稿"
+        expect(page).to have_content "ログインしました"
       end
     end
     context "ログインしているユーザーが" do
       it "ログアウトしたらログアウトできる" do
         create(:user_introduction)
-        visit new_user_session_path
-        fill_in "user[email]", with: "test@gmail"
-        fill_in "user[password]", with: "111111"
-        click_on "commit"
+        user_login
         click_on find(".nav-item5").text
-        expect(page).to have_content "オンライン上で御朱印を勝手に認定します"
+        expect(page).to have_content "ログアウトしました"
       end
     end
     context "ログインしているユーザーが" do
       it "退会ボタンを押すことで退会される" do
         create(:user_introduction)
-        visit new_user_session_path
-        fill_in "user[email]", with: "test@gmail"
-        fill_in "user[password]", with: "111111"
-        click_on "commit"
+        user_login
         find("#dropdownMenuButton").click
         page.accept_confirm do
           click_on page.all(".dropdown-item")[3].text 
