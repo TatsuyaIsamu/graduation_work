6e9a6efa17503ecadc0fa817183eb3ac09e650ad
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-14 23:41:48 +0900

    提出前の最終調整

--- db/fixtures/development/admin_user.rb
+++ db/fixtures/development/admin_user.rb
@@ -1,30 +1,29 @@
 
-  # User.seed(
-  #   :id,
-  #   { id: 1, name: "admin_user", email: "admin@gmail.com", admin: true, password: 'password' },
-  # )
+  User.seed(
+    :id,
+    { id: 1, name: "admin_user", email: "admin@gmail.com", admin: true, password: 'password' },
+  )
 
-  # UserIntroduction.seed(
-  #   :id,
-  #   {
-  #     id: 1,
-  #     user_id: 1,
-  #     address: 0,  
-  #   }
-  # )
-  # 3.times do |n|
-  #   Ranking.seed(
-  #     :id,{
-  #       id: n+1,
-  #       rank: n+1,
-  #       user_id: 1
-  #     }
-  #   )
-  #   RankingItem.seed(
-  #     :id , {
-  #       id: n+1,
-  #       ranking_id: n+1,
-  #       shinto_id: shinto_rand
-  #     }
-  #   )
-  # end
+  UserIntroduction.seed(
+    :id,
+    {
+      id: 1,
+      user_id: 1,
+      address: 0,  
+    }
+  )
+  3.times do |n|
+    Ranking.seed(
+      :id,{
+        id: n+1,
+        rank: n+1,
+        user_id: 1
+      }
+    )
+    RankingItem.seed(
+      :id , {
+        id: n+1,
+        ranking_id: n+1,
+      }
+    )
+  end
