5721ed47f85c9fea186d34414a3727aac56beced
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-15 22:15:45 +0900

    本番環境のメールをletter_opener で設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   root  "tops#index"
