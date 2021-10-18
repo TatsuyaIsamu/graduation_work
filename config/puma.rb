b00754b2e12616656781539a21bfaabfde23ae07
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-18 12:14:52 +0900

    環境を本番に移行

--- config/puma.rb
+++ config/puma.rb
@@ -13,13 +13,13 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+# environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # 本番環境時に以下をアクティブにする
-# environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 
 # Specifies the number of `workers` to boot in clustered mode.
