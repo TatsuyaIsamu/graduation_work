2691d105c6474f86ddc91cc0068c74edad1a4df6
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-23 17:19:49 +0900

    無駄なコメントアウトを削除

--- config/puma.rb
+++ config/puma.rb
@@ -6,17 +6,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 
 # 開発環境
-# environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 port        ENV.fetch("PORT") { 3000 }
 
 # 本番環境時
-environment ENV.fetch("RAILS_ENV") { "production" }
+# environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-# Docker環境
-app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+# ローカル使用の際は以下をコメントアウト
+# app_root = File.expand_path("../..", __FILE__)
+# bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+# stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
