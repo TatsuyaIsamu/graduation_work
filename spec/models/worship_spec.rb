17f8d2e6f2d86002d60ab802080c3e91fb1d5b87
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-25 00:31:31 +0900

    テストの実装

--- spec/models/worship_spec.rb
+++ spec/models/worship_spec.rb
@@ -3,10 +3,20 @@ RSpec.describe Worship, type: :model do
     it '参拝インスタンスを作成できる' do
       expect(create(:worship)).to be_valid
     end
+    it '参拝が今日のインスタンスを作成できる' do
+      expect(create(:worship, worship_day: Date.today)).to be_valid
+    end
     context '参拝日が空のインスタンスを作成しようとしたとき' do
       it 'バリデーションにかかる' do
         worship = create(:worship)
         worship.worship_day = nil
+        expect(worship).to be_invalid
+      end
+    end
+    context '参拝日が明日以降のインスタンスを作成しようとしたとき' do
+      it 'バリデーションにかかる' do
+        worship = create(:worship)
+        worship.worship_day = Date.today + 1
         expect(worship).to be_invalid
       end
     end
