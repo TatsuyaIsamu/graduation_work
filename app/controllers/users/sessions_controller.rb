7cf7e2bbbc79029506ac1cbb527131fae2383e11
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-25 17:03:20 +0900

    デザインを調整

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,32 +9,4 @@ class Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to home_path, notice: 'ゲストユーザーとしてログインしました。'
   end
-
-  def guest_admin_sign_in
-    user = User.admin_guest
-    sign_in user
-    redirect_to home_path, notice: '管理者ゲストユーザーとしてログインしました。'
-  end
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
