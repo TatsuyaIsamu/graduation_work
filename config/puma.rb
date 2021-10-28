9a792f07b74dbe7de0ffc32bd0ec1d04a99f32b5
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-10-28 15:13:29 +0900

    ajax化

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,7 @@ environment ENV.fetch("RAILS_ENV") { "production" }
 plugin :tmp_restart
 
 # ローカル使用の際は以下をコメントアウト
-app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+# app_root = File.expand_path("../..", __FILE__)
+# bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+# stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
